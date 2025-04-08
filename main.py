a176d036b671e6933bb0f3ab6c93e8de75992c41
Alihor <biglazycat03@gmail.com>
2025-04-08 21:41:12 +0500

    Изменил ГТУ

--- main.py
+++ main.py
@@ -39,19 +39,24 @@ canvas = Canvas(root, bg='black', highlightthickness=0)
 canvas.place(x=0, y=0, width=w, height=h)  # Растягиваем на весь экран
 
 # Установки ГТУ
-square1 = canvas.create_rectangle(0, 0, 200, 200, fill='blue', outline='white')
-square1 = canvas.create_rectangle(200, 0, 400, 200, fill='red', outline='white')
-square1 = canvas.create_rectangle(400, 0, 600, 200, fill='red', outline='white')
-square1 = canvas.create_rectangle(600, 0, 800, 200, fill='red', outline='white')
-square1 = canvas.create_rectangle(800, 0, 1000, 200, fill='red', outline='white')
-square1 = canvas.create_rectangle(1000, 0, 1200, 200, fill='red', outline='white')
-square1 = canvas.create_rectangle(1200, 0, 1400, 200, fill='red', outline='white')
-square1 = canvas.create_rectangle(1400, 0, 1600, 200, fill='red', outline='white')
-square1 = canvas.create_rectangle(1600, 0, 1800, 200, fill='red', outline='white')
+margin = 10 #Отступы
+size = 150 #Размер
+x = 10
+y = 10
+canvas.create_rectangle(x, y, x + size, y + size, fill='blue', outline='white')
+canvas.create_rectangle(x + size + margin, y, x + (size * 2) + margin, y + size, fill='blue', outline='white')
+canvas.create_rectangle(x + (size * 2) + margin * 2, y, x + (size * 3) + margin, y + size, fill='blue', outline='white')
+canvas.create_rectangle(x + (size * 3) + margin * 2, y, x + (size * 4) + margin, y + size, fill='blue', outline='white')
+canvas.create_rectangle(x + (size * 4) + margin * 2, y, x + (size * 5) + margin, y + size, fill='blue', outline='white')
+canvas.create_rectangle(x + (size * 5) + margin * 2, y, x + (size * 6) + margin, y + size, fill='blue', outline='white')
+canvas.create_rectangle(x + (size * 6) + margin * 2, y, x + (size * 7) + margin, y + size, fill='blue', outline='white')
+canvas.create_rectangle(x + (size * 7) + margin * 2, y, x + (size * 8) + margin, y + size, fill='blue', outline='white')
+canvas.create_rectangle(x + (size * 8) + margin * 2, y, x + (size * 9) + margin, y + size, fill='blue', outline='white')
+
+
 
 
 
 download_button = Button(root, text='Загрузить данные', bg='white', command=open_file)
 download_button.place(relx=0.95, rely=0.95, anchor=SE) #использовал rely relx
-
 root.mainloop()
