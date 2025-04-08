020cd83b2ba1d657e2f82be62b345306c11ebcfd
crizis42 <garimil86@gmail.com>
2025-04-08 20:24:10 +0500

    добавлено появление диалогового окна при загрузке файла

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from tkinter import *
+from tkinter.filedialog import askopenfilename
+from tkinter.messagebox import showinfo, showerror
 
-def fullscreen_on(event):
-    root.attributes('-fullscreen', True)
-
-def fullscreen_off(event):
-    root.attributes('-fullscreen', False)
 
 root = Tk()
 
@@ -16,10 +13,32 @@ h = root.winfo_screenheight()-80
 
 root.geometry(f'{w}x{h}+0+0')
 
+
+def fullscreen_on(event):
+    root.attributes('-fullscreen', True)
+
+def fullscreen_off(event):
+    root.attributes('-fullscreen', False)
+
+def open_file():
+    filepath = askopenfilename()
+    if filepath != "":
+        with open(filepath, 'r', encoding='utf8') as file:
+            try:
+                print(filepath)
+                showinfo("Успех!", "Файл успешно загружен.")
+            except Exception:
+                showerror("Ошибка!", "Ошибка при чтении файла.")
+
+
 root.bind('<f>', fullscreen_on)
 root.bind('<Escape>', fullscreen_off)
 
-download_button = Button(root, text='Загрузить данные', bg='white')
+download_button = Button(text='Загрузить данные', bg='white', command=open_file)
 download_button.place(x=w-120,y=h)
+
+
+
+
 
 root.mainloop()
