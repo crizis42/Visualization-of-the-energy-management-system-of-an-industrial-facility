8ad8affc2f57bbc169399a533858c154620550b1
crizis42 <garimil86@gmail.com>
2025-04-08 19:37:56 +0500

    window added, download button added

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-print("a4dev")
-a4dev = 'Cool guys'
-Эмиль что-то показывает Сашке
+from tkinter import *
+
+def fullscreen_on(event):
+    root.attributes('-fullscreen', True)
+
+def fullscreen_off(event):
+    root.attributes('-fullscreen', False)
+
+root = Tk()
+
+root['bg'] = 'black'
+root.title('Визуализация системы')
+
+w = root.winfo_screenwidth()
+h = root.winfo_screenheight()-80
+
+root.geometry(f'{w}x{h}+0+0')
+
+root.bind('<f>', fullscreen_on)
+root.bind('<Escape>', fullscreen_off)
+
+download_button = Button(root, text='Загрузить данные', bg='white')
+download_button.place(x=w-120,y=h)
+
+root.mainloop()
